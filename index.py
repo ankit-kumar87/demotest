97c53864ac777dae4901ba1951dcd963739f39db
Ankit Kumar <ankit.kumar@tangence.com>
2025-07-12 01:02:39 +0530

    added new some code in pyhton

--- index.py
+++ index.py
@@ -1,8 +1,2 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+print("Hello, World!")
 
-num = int(input("Enter a number: "))
-print("Factorial:", factorial(num))
